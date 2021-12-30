9a81869eb00667e62e210178631b3533ed7c3413
Flavio Aro <35077695+FlavioAro@users.noreply.github.com>
2021-12-29 23:43:36 -0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyC1s_MIucbtwVHGgLAeOQgKKMGI-FLcRGM",
-  authDomain: "react-contact-fae17.firebaseapp.com",
-  databaseURL: "https://react-contact-fae17-default-rtdb.firebaseio.com",
-  projectId: "react-contact-fae17",
-  storageBucket: "react-contact-fae17.appspot.com",
-  messagingSenderId: "843953336059",
-  appId: "1:843953336059:web:094ef2692042f81fc94a5e",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const fireDb = firebase.initializeApp(firebaseConfig);
